1bd005912d94fdf46f0d2f7f3a5dc8b52897456d
dbelgrod <davidbelgrod@gmail.com>
2021-11-01 23:21:38 -0400

    generated 8 points for ee & vf

--- app/main.cu
+++ app/main.cu
@@ -18,6 +18,7 @@
 #include <gpubf/util.cuh>
 #include <gpubf/io.cuh>
 
+#define Vec3Conv(v) {v[0], v[1], v[2]}
 
 // #include <gpubf/klee.cuh>
 
@@ -25,7 +26,9 @@ using namespace std;
 
 void addData(
     const Aabb &a, 
-    const Aabb&b, 
+    const Aabb &b, 
+    const Eigen::MatrixXd& V0,
+    const Eigen::MatrixXd& V1,
     std::vector<std::array<std::array<float, 3>, 8>>& queries)
 {
     auto is_vertex = [&](Aabb x){return x.vertexIds.y < 0 ;};
@@ -34,36 +37,45 @@ void addData(
 
     if (is_vertex(a) && is_face(b))
     {
+        auto avids = a.vertexIds;
+        auto bvids = b.vertexIds;
             // Point at t=0
-        // auto vertex_start = V0.row(vi);
+        auto vertex_start = V0.cast<float>().row(avids.x);
         // // Triangle at t = 0
-        // auto face_vertex0_start = V0.row(F(fi, 0));
-        // auto face_vertex1_start = V0.row(F(fi, 1));
-        // auto face_vertex2_start = V0.row(F(fi, 2));
+        auto face_vertex0_start = V0.cast<float>().row(bvids.x);
+        auto face_vertex1_start = V0.cast<float>().row(bvids.y);
+        auto face_vertex2_start = V0.cast<float>().row(bvids.z);
         // // Point at t=1
-        // auto vertex_end = V1.row(vi);
+        auto vertex_end = V1.row(avids.x);
         // // Triangle at t = 1
-        // auto face_vertex0_end = V1.row(F(fi, 0));
-        // auto face_vertex1_end = V1.row(F(fi, 1));
-        // auto face_vertex2_end = V1.row(F(fi, 2));
+        auto face_vertex0_end = V1.cast<float>().row(bvids.x);
+        auto face_vertex1_end = V1.cast<float>().row(bvids.y);
+        auto face_vertex2_end = V1.cast<float>().row(bvids.z);
+
+        std::array<std::array<float, 3>, 8> tmp;
+        tmp[0] = Vec3Conv(vertex_start);
 
     }
     else if (is_face(a) && is_vertex(b))
-        return addData(b,a, queries);
+        return addData(b, a, V0, V1, queries);
     else if (is_edge(a) && is_edge(b))
     {
-    //     // Edge 1 at t=0
-    // auto edge0_vertex0_start = V0.row(E(ei, 0));
-    // auto edge0_vertex1_start = V0.row(E(ei, 1));
-    // // Edge 2 at t=0
-    // auto edge1_vertex0_start = V0.row(E(ej, 0));
-    // auto edge1_vertex1_start = V0.row(E(ej, 1));
-    // // Edge 1 at t=1
-    // auto edge0_vertex0_end = V1.row(E(ei, 0));
-    // auto edge0_vertex1_end = V1.row(E(ei, 1));
-    // // Edge 2 at t=1
-    // auto edge1_vertex0_end = V1.row(E(ej, 0));
-    // auto edge1_vertex1_end = V1.row(E(ej, 1));
+        auto avids = a.vertexIds;
+        auto bvids = b.vertexIds;
+        //     // Edge 1 at t=0
+        auto edge0_vertex0_start = V0.cast<float>().row(avids.x);
+        auto edge0_vertex1_start = V0.cast<float>().row(avids.y);
+        // // Edge 2 at t=0
+        auto edge1_vertex0_start = V0.cast<float>().row(bvids.x);
+        auto edge1_vertex1_start = V0.cast<float>().row(bvids.y);
+        // // Edge 1 at t=1
+        auto edge0_vertex0_end = V1.cast<float>().row(avids.x);
+        auto edge0_vertex1_end = V1.cast<float>().row(avids.y);
+        // // Edge 2 at t=1
+        auto edge1_vertex0_end = V1.cast<float>().row(bvids.x);
+        auto edge1_vertex1_end = V1.cast<float>().row(bvids.y);
+        
+        // queries.emplace_back(Vec3Conv(edge0_vertex0_start));
     }
 }
 
@@ -76,7 +88,13 @@ int main( int argc, char **argv )
     const char* filet1 = argv[2];
     
     vector<Aabb> boxes;
-    parseMesh(filet0, filet1, boxes);
+    Eigen::MatrixXd vertices_t0;
+    Eigen::MatrixXd vertices_t1;
+    Eigen::MatrixXi faces; 
+    Eigen::MatrixXi edges;
+
+    parseMesh(filet0, filet1, vertices_t0, vertices_t1, faces, edges);
+    constructBoxes(vertices_t0, vertices_t1, faces, edges, boxes);
     int N = boxes.size();
     int nbox = 0;
     
@@ -113,6 +131,8 @@ int main( int argc, char **argv )
 
         Aabb a = boxes[aid];
         Aabb b = boxes[bid];  
+
+        addData(a, b, vertices_t0, vertices_t1, queries);
     
     }
 
