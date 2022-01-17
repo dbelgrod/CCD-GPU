82abe390d0f8c9f9f00b2ddec521ffe5b7083a4b
dbelgrod <davidbelgrod@gmail.com>
2022-01-16 22:10:31 -0500

    removing device reset since it eats a lot of time

--- src/helper.cu
+++ src/helper.cu
@@ -598,6 +598,6 @@ void run_ccd(const vector<Aabb> boxes, const Eigen::MatrixXd &vertices_t0,
   cout << "toi " << toi << endl;
   cudaFree(d_ee_data_list);
   cudaFree(d_vf_data_list);
-  cudaDeviceReset();
+  // cudaDeviceReset();
   cudaDeviceSynchronize();
 }
