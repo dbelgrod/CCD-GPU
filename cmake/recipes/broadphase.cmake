d7f2847bfb388818ad9392a30a515bd65600e49e
dbelgrod <davidbelgrod@gmail.com>
2022-06-30 15:22:38 -0400

    changing bp commit id

--- cmake/recipes/broadphase.cmake
+++ cmake/recipes/broadphase.cmake
@@ -4,8 +4,8 @@ endif()
 
 message(STATUS "Third-party: creating target 'STQ::CUDA'")
 
-option(STQ_WITH_CPU  "Enable CPU Implementation"  OFF)
-option(STQ_WITH_CUDA "Enable CUDA Implementation"  ON)
+option(STQ_WITH_CPU "Enable CPU Implementation" OFF)
+option(STQ_WITH_CUDA "Enable CUDA Implementation" ON)
 
 if(EXISTS "${CCDGPU_BROADPHASE_PATH}")
     message(STATUS "Using Broadphase found at: ${CCDGPU_BROADPHASE_PATH}")
@@ -15,7 +15,7 @@ else()
     FetchContent_Declare(
         broadphase
         GIT_REPOSITORY https://github.com/dbelgrod/broadphase-gpu.git
-        GIT_TAG fea5f45221400a7a70821ddbafd1f6918553d22c
+        GIT_TAG d657455b4a8282438b541569d2c43de9b4bda9f9
         GIT_SHALLOW FALSE
     )
     FetchContent_MakeAvailable(broadphase)
