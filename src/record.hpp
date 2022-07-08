70f45313718c997fb94f66da34ce5b92e4983cbd
dbelgrod <davidbelgrod@gmail.com>
2022-07-08 11:44:19 -0400

    getting time in milliseconds now

--- src/record.hpp
+++ src/record.hpp
@@ -62,8 +62,7 @@ struct Record {
     float elapsed; // was double
     if (!gpu_timer_on) {
       timer.stop();
-      elapsed += timer.getElapsedTimeInMicroSec();
-      elapsed /= 1000.f;
+      elapsed += timer.getElapsedTimeInMilliSec();
       spdlog::trace("Cpu timer stopped for {}: {:.6f} ms", tag, elapsed);
     } else {
       cudaEventRecord(stop);
